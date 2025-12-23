201eb5faba5af60a8896b453244363eabe70702b
Carlo van Overbeek <cvanoverbeek@schubergphilis.com>
2025-12-23 15:44:57 +0100

    fix: catch application logs in lambda context

--- src/clisechubman/aws_lambda_handler.py
+++ src/clisechubman/aws_lambda_handler.py
@@ -1,8 +1,10 @@
 from aws_lambda_powertools import Logger
+from aws_lambda_powertools.logging import utils
 
 from .clisechubman import _apply_rules
 
 LOGGER = Logger()
+utils.copy_config_to_registered_loggers(source_logger=LOGGER)
 
 
 @LOGGER.inject_lambda_context(log_event=True)
